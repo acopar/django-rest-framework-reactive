33e703b603a9815a40e91f2a0995a580003562b8
Andrej Copar <andrej@genialis.com>
2025-07-08 13:25:30 +0000

    Drop support for Python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         'src', exclude=['tests', 'tests.*', '*.tests', '*.tests.*']
     ),
     package_dir={'': 'src'},
-    python_requires='>=3.6, <3.11',
+    python_requires='>=3.8, <3.11',
     install_requires=[
         'Django~=3.2.12',
         'djangorestframework~=3.13.1',
@@ -69,8 +69,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
